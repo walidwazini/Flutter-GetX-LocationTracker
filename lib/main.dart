240ad8f22b6c6b1aabebca85ced6f34aa44f95ec
Walid Wazini <wazini25@gmail.com>
2022-01-04 06:18:34 +0800

    pub get packages

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      theme: ThemeData(
+        // primarySwatch: Colors.blueAccent.shade400,
+        primaryColor: Color(0xff00b7ab)
+      ),
+      home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text('Start'),
+            ElevatedButton(
+              child: Icon(Icons.add),
+              onPressed: (){},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
